9c98f328f50c12711c4ac1fc060817d6c232deab
ponpoko-unagi <finanprog.ryota112@gmail.com>
2020-08-05 12:45:06 +0900

    Youtube Data API Demo

--- work/js/nakashima.js
+++ work/js/nakashima.js
@@ -1,6 +1,28 @@
-let app = new Vue({
+var vm = new Vue({
   el: '#app',
   data: {
-    message: '中島さん'
+    results: null,
+    keyword: null,
+    params: {
+      q: '',
+      part: 'snippet',
+      type: 'video',
+      maxResults: '10',
+      key: '取得したAPIキー'
+    }
+  },
+  methods: {
+    searchMovies: function () {
+      this.params.q = this.keyword;
+      var self = this;
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {params: this.params})
+        .then(function (res) {
+          self.results = res.data.items;
+        })
+        .catch(function (err) {
+          console.log(err)
+        });
+    }
   }
 });
