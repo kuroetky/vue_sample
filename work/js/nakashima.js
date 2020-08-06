a95ef8bc9fd1f42a303abeb3f77d6704caa99181
ponpoko-unagi <finanprog.ryota112@gmail.com>
2020-08-07 01:49:52 +0900

    チャンネルの統計情報を取得するように修正

--- work/js/nakashima.js
+++ work/js/nakashima.js
@@ -1,28 +1,50 @@
+var API_KEY = 'AIzaSyAZrymJcLspFg46RLEupb_n-JEz68yNDh8'
 var vm = new Vue({
   el: '#app',
   data: {
     results: null,
-    keyword: null,
     params: {
-      q: '',
-      part: 'snippet',
-      type: 'video',
-      maxResults: '10',
-      key: '***********************'
+      channel: {
+        q: '',
+        part: 'snippet',
+        type: 'channel',
+        maxResults: '5',
+        key: API_KEY
+      },
+      statistics: {
+        part: 'snippet,statistics',
+        id: '',
+        key: API_KEY
+      }
     }
   },
   methods: {
-    searchMovies: function () {
-      this.params.q = this.keyword;
-      var self = this;
+    searchChannels: function () {
+      var own = this;
       axios
-        .get('https://www.googleapis.com/youtube/v3/search', {params: this.params})
+        .get('https://www.googleapis.com/youtube/v3/search', {params: this.params.channel})
         .then(function (res) {
-          console.log(res)
-          self.results = res.data.items;
+          var channelIds = [];
+          for(item of res.data.items) {
+            channelIds.push(item.id.channelId);
+          }
+          console.log(channelIds);
+          own.searchChannelStatistics(channelIds);
         })
         .catch(function (err) {
-          console.log(err)
+          console.log(err);
+        });
+    },
+    searchChannelStatistics: function (channelIds) {
+      this.params.statistics.id = channelIds.join(',');
+      var own = this;
+      axios
+        .get('https://www.googleapis.com/youtube/v3/channels', {params: this.params.statistics})
+        .then(function (res) {
+          own.results = res.data.items;
+        })
+        .catch(function (err) {
+          console.log(err);
         });
     }
   }
